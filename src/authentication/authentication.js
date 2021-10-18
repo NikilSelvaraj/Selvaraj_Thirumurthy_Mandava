6ab5947b9b2f127c235bd704f66106c1a2cb0c17
sujitrajt <sujitrajt@gmail.com>
2021-10-17 19:12:50 -0500

    visitor, user changes

--- src/authentication/authentication.js
+++ src/authentication/authentication.js
@@ -6,6 +6,6 @@ function login(element) {
     } else if(userType === 'user') { 
         window.location.href = '../Users/users.html';
     } else if (userType === 'visitor') {
-        window.location.href = '../Visitor/vistor.html'
+        window.location.href = '../Visitor/visitor.html'
     }
 }
