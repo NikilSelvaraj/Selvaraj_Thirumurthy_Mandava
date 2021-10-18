1b7834c49eccb48c216b3c8ee256e081dba134a4
Nikil Nandha Selvaraj <nikilselvaraj07@gmail.com>
2021-10-17 19:51:01 -0500

    comments and manage admin section

--- src/authentication/authentication.js
+++ src/authentication/authentication.js
@@ -1,9 +1,10 @@
+// navigate to different pages based on user type
 function login(element) {
     let userType = element.value;
     localStorage.setItem('userType', userType);
-    if(userType === 'manager' || userType === 'admin') {
+    if (userType === 'manager' || userType === 'admin') {
         window.location.href = '../Manager/manager.html';
-    } else if(userType === 'user') { 
+    } else if (userType === 'user') {
         window.location.href = '../Users/users.html';
     } else if (userType === 'visitor') {
         window.location.href = '../Visitor/vistor.html'
