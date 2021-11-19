ec1336569009b2b82de828046f8fa801a86dea12
Nikil Nandha Selvaraj <nikilselvaraj07@gmail.com>
2021-11-18 21:34:24 -0600

    added env

--- src/authentication/login/login.js
+++ src/authentication/login/login.js
@@ -1,12 +1,43 @@
-import React, {useEffect} from "react";
+import React, { useState , useEffect } from 'react';
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import login from '../../assets/images/login.png'
 import Registration from "../registration/registration";
+import axios from 'axios';
 function Login() {
     useEffect(() => {
         document.getElementsByClassName('nav-item active')[0].classList.remove('active');
         document.getElementById('authenticationTab').classList.add('active');
     });
+    const [state,setState] = useState({});
+    const [credentials, setCredentials] = useState({username:'',password:'',forgotPassword:false});
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setCredentials({...credentials,[name]:value })
+    }
+
+    function onSignin(event) {
+        event.preventDefault();
+        axios({
+            method: 'post',
+            url: process.env.REACT_APP_API_PATH + '/loginUser.php',
+            headers: {
+                'content-type': 'application/json'
+            },
+            data: credentials
+        })
+            .then(result => {
+                console.log(result.data)
+                setState({
+                    dataSent: result.data.sent,
+                });
+            })
+            .catch(error => {
+                setState({
+                    error: error.message
+                });
+                console.log(state['error']);
+            });
+    }
     return (
         <Router>
             <Switch>
@@ -19,12 +50,13 @@ function Login() {
                         </div>
                         <div className="login-container">
                             <div className="d-flex justify-center login-header font-oswald">Welcome back</div>
-                            <form className="d-flex flex-direction-column w-100" onSubmit={signIn}>
+                            <form className="d-flex flex-direction-column w-100" onSubmit={onSignin}>
                                 <div className="d-flex flex-direction-column">
-                                    <input type="email" id="email" name="email" placeholder="Username / Email"
-                                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" size="30" required />
+                                    <input type="email" id="email" name="username" placeholder="Email"
+                                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" size="30" required 
+                                        value={credentials['username']} onChange={handleChange}/>
                                     <input type="password" id="password" name="password" placeholder="Password"
-                                        minLength="8" required />
+                                        minLength="8" required value={credentials['password']} onChange={handleChange}/>
                                 </div>
                                 <div className="d-flex flex-direction-column align-items-center">
                                     <button className="btn login-btn" type="submit">Sign in</button>
@@ -39,24 +71,6 @@ function Login() {
             </Switch>
         </Router>
     )
-
-
-    function signIn(event) {
-        event.preventDefault();
-        var emailValue = document.getElementById('email').value;
-        if (emailValue) {
-            if (emailValue === 'admin@instawash.com') {
-                 window.location = '/admin'
-            }
-            else if (emailValue === 'manager@instawash.com') {
-                 window.location = '/manager'
-            } else if (emailValue === 'user@instawash.com') {
-                window.location = '/user'
-            } else {
-                window.location = '/visitor'
-            }
-        }
-    }
 }
 
 export default Login;
--- src/authentication/registration/registration.js
+++ src/authentication/registration/registration.js
@@ -1,41 +1,77 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import signup from '../../assets/images/signup.png'
 import Authentication from "../authentication";
+import axios from 'axios';
 function Registration() {
+    const [state, setState] = useState({});
+    const [registrationInfo, setRegistrationInfo] = useState({ firstName: '', lastName: '', email: '', password: '', confirmPassword: '' });
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setRegistrationInfo({ ...registrationInfo, [name]: value })
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        axios({
+            method: 'post',
+            url: process.env.REACT_APP_API_PATH + '/registerUser.php',
+            headers: {
+                'content-type': 'application/json'
+            },
+            data: registrationInfo
+        })
+            .then(result => {
+                console.log(result.data)
+                setState({
+                    dataSent: result.data.sent,
+                });
+            })
+            .catch(error => {
+                setState({
+                    error: error.message
+                });
+                console.log(state['error']);
+            });
+    }
     return (
         <Router>
-        <Switch>
-        <Route exact path='/registration'>
-        {/* registration section */}
-        <div className="d-flex flex-direction-row justify-around registration-section fade">
-            <div className="registration-container">
-                <div className="d-flex justify-center registration-header font-oswald">Create an account</div>
-                <form className="d-flex flex-direction-column w-100" method="post" encType="text/html">
-                    <div className="d-flex flex-direction-column">
-                        <div className="d-flex flex-direction-row justify-around">
-                            <input type="text" className="name-input" id="fname" name="fname" placeholder="First Name" maxLength="60" required/>
-                            <input type="text" className="name-input" id="lname" name="lname" placeholder="Last Name" maxLength="60"  required/>
+            <Switch>
+                <Route exact path='/registration'>
+                    {/* registration section */}
+                    <div className="d-flex flex-direction-row justify-around registration-section fade">
+                        <div className="registration-container">
+                            <div className="d-flex justify-center registration-header font-oswald">Create an account</div>
+                            <form className="d-flex flex-direction-column w-100" onSubmit={onSubmit}>
+                                <div className="d-flex flex-direction-column">
+                                    <div className="d-flex flex-direction-row justify-around">
+                                        <input type="text" className="name-input" id="fname" name="firstName" placeholder="First Name" maxLength="60"
+                                            required value={registrationInfo['firstName']} onChange={handleChange} />
+                                        <input type="text" className="name-input" id="lname" name="lastName" placeholder="Last Name" maxLength="60"
+                                            required value={registrationInfo['lastName']} onChange={handleChange} />
+                                    </div>
+                                    <input type="email" id="email" name="email" placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                                        value={registrationInfo['email']} onChange={handleChange} />
+                                    <input type="password" id="password" name="password" placeholder="Password" required minLength='8'
+                                        value={registrationInfo['password']} onChange={handleChange} />
+                                    <input type="password" id="confirm_password" name="confirmPassword" placeholder="Confirm Password" required minLength='8'
+                                        value={registrationInfo['confirmPassword']} onChange={handleChange} />
+                                </div>
+                                <div className="d-flex flex-direction-column align-items-center">
+                                    <button className="btn register-btn" type="submit">Sign up</button>
+                                    <br />
+                                    <p>Already have an account? <Link className='link-style'
+                                        to="/authentication">Login</Link> here</p>
+                                </div>
+                            </form>
                         </div>
-                        <input type="email" id="email" name="email" placeholder="Email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
-                        <input type="password" id="password" name="password" placeholder="Password" required minLength='8'/>
-                        <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" required minLength='8'/>
+                        <div className="image-container">
+                            <img src={signup} width="550px" height="500px" alt="registrationimage" />
+                        </div>
                     </div>
-                    <div className="d-flex flex-direction-column align-items-center">
-                        <button className="btn register-btn" type="submit">Sign up</button>
-                        <br />
-                        <p>Already have an account? <Link className='link-style'
-                            to="/authentication">Login</Link> here</p>
-                    </div>
-                </form>
-            </div>
-            <div className="image-container">
-                <img src={signup} width="550px" height="500px" alt="registrationimage"/>
-            </div>
-        </div>
-        </Route>
-        <Route exact path='/authentication'><Authentication/></Route>
-        </Switch>
+                </Route>
+                <Route exact path='/authentication'><Authentication /></Route>
+            </Switch>
         </Router>
     )
 }
